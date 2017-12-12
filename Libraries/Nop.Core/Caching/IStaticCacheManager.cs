e9b26721f589083d8ab6c34b8e647d971abf156f
Kajornjit Songsaen <kajornjit@gmail.com>
2017-12-12 12:55:05 +0700

    update readme

--- Libraries/Nop.Core/Caching/IStaticCacheManager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nop.Core.Caching
-{
-    /// <summary>
-    /// Represents a manager for caching between HTTP requests (long term caching)
-    /// </summary>
-    public interface IStaticCacheManager : ICacheManager
-    {
-    }
-}
--- Presentation/Nop.Web/Areas/Admin/Models/Home/DashboardModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nop.Web.Framework.Mvc.Models;
-
-namespace Nop.Web.Areas.Admin.Models.Home
-{
-    public partial class DashboardModel : BaseNopModel
-    {
-        public bool IsLoggedInAsVendor { get; set; }
-    }
-}
